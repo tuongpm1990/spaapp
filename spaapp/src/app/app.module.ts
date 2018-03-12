b2f6059a75b27e5c23fbcd9c9a12ace81ac7ea7e
tuongpm1990 <tuongpm@hongngochospital.vn>
2018-03-13 01:16:38 +0700

    add devextreme

--- spaapp/src/app/app.module.ts
+++ spaapp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DxButtonModule } from 'devextreme-angular';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DxButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
